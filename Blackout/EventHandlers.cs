8ee364a833e264b38667712a4efd10ce72937242
Luke Janik <luke04janik@gmail.com>
2018-12-23 20:47:38 -0600

    clean Plugin by splitting into multiple files

---
+++ Blackout/EventHandlers.cs
@@ -0,0 +1,15 @@
+﻿using Smod2.EventHandlers;
+using Smod2.Events;
+
+namespace Blackout
+{
+    public class EventHandlers : IEventHandlerRoundStart
+    {
+        public void OnRoundStart(RoundStartEvent ev)
+        {
+            Plugin.validRanks = Plugin.instance.GetConfigList("blackout_ranks");
+
+            Plugin.giveFlashlights = Plugin.instance.GetConfigBool("blackout_flashlights");
+        }
+    }
+}
--- Blackout/Plugin.cs
+++ Blackout/Plugin.cs
@@ -1,12 +1,6 @@
-﻿using System.Linq;
-using scp4aiur;
-using Smod2;
-using Smod2.API;
+﻿using scp4aiur;
 using Smod2.Attributes;
-using Smod2.Commands;
 using Smod2.Config;
-using Smod2.EventHandlers;
-using Smod2.Events;
 
 namespace Blackout
 {
@@ -44,85 +38,6 @@ namespace Blackout
 
         public override void OnDisable()
         {
-        }
-    }
-
-    public class EventHandlers : IEventHandlerRoundStart
-    {
-        public void OnRoundStart(RoundStartEvent ev)
-        {
-            Plugin.validRanks = Plugin.instance.GetConfigList("blackout_ranks");
-
-            Plugin.giveFlashlights = Plugin.instance.GetConfigBool("blackout_flashlights");
-        }
-    }
-
-    public class CommandHandler : ICommandHandler
-    {
-        private bool run;
-
-        public string[] OnCall(ICommandSender sender, string[] args)
-        {
-            bool valid = sender is Server;
-
-            Player player = sender as Player;
-            if (!valid && player != null)
-            {
-                valid = Plugin.validRanks.Contains(player.GetRankName());
-            }
-
-            if (valid)
-            {
-                run = !run;
-
-                if (run)
-                {
-                    if (Plugin.giveFlashlights)
-                    {
-                        foreach (Player p in PluginManager.Manager.Server.GetPlayers())
-                        {
-                            if (!p.HasItem(ItemType.FLASHLIGHT))
-                            {
-                                p.GiveItem(ItemType.FLASHLIGHT);
-                            }
-                        }
-                    }
-
-                    PlayerManager.localPlayer.GetComponent<MTFRespawn>().CallRpcPlayCustomAnnouncement("LIGHT SYSTEM SCP079RECON6", false);
-
-                    Timing.Timer(TenSecondBlackout, 8.7f);
-                }
-
-                return new[]
-                {
-                    "Toggled blackout"
-                };
-            }
-
-            return new[]
-            {
-                $"You (rank {player.GetRankName()}) do not have permissions to that command."
-            };
-        }
-
-        private void TenSecondBlackout(float inaccuracy = 0)
-        {
-            Generator079.generators[0].CallRpcOvercharge();
-
-            if (run)
-            {
-                Timing.Timer(TenSecondBlackout, 11 + inaccuracy);
-            }
-        }
-
-        public string GetUsage()
-        {
-            return "blackout";
-        }
-
-        public string GetCommandDescription()
-        {
-            return "Causes all the lights to flicker.";
         }
     }
 }
